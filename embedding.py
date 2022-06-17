f482ac91cd63dd48c63e935e7c99629bb5413c1c
simao17 <simiasmonteiro@gmail.com>
2022-06-17 17:19:35 +0100

    fc

--- embedding.py
+++ embedding.py
@@ -1,13 +1,21 @@
 import gensim
 import csv
-from gensim.models.doc2vec import TaggedDocument, Doc2Vec 
+from gensim.models import Word2Vec 
 
-def train_model():
-    with open('Tweets_pt_pt.csv', 'r',encoding="utf-8") as file:
-        tweets = csv.DictReader(file, skipinitialspace=True)  
-        documents = [TaggedDocument(text['tweet_text'].split(), [text['id']])  for text in tweets]
-    model = Doc2Vec(documents, vector_size=100, window=8, min_count=2, workers=7)
-    model.distance('😀','😄')
-    return model
+f2=open("tweets.txt",'w')
 
+with open('Tweets_pt_pt.csv', 'r',encoding="utf-8") as file:
+    tweets = csv.DictReader(file, skipinitialspace=True)  
+    for tweet in tweets:
+        f2.write(tweet['tweet_text']+'\n')
+f2.close()
 
+file=open("tweets.txt",encoding="utf-8")
+
+frases=[]
+for line in file:
+    frases.append(list(gensim.utils.tokenize(line, lower=True)))
+
+model = Word2Vec(sentences=frases,sg=0, workers=5, epochs=20, vector_size=100)
+
+print(model.wv.most_similar("like"))
